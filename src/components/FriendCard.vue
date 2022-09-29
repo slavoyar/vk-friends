a649c4390a222c0861d7dfc92e642b4d619a3dab
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2022-09-30 02:51:31 +0300

    calculate age and gender

--- src/components/FriendCard.vue
+++ src/components/FriendCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="friend-card">
-    <img src="" alt="" />
-    <span>FirstName LastName</span>
-    <span>Sex: male/female</span>
-    <span>Age: 20</span>
-    <span>Friends: 83</span>
+    <img :src="user?.photo_100" alt="" />
+    <span>{{ user?.first_name }} {{ user?.last_name }}</span>
+    <span>Sex: {{ getSex }}</span>
+    <span>Age: {{ getAge }}</span>
+    <span>Friends: {{ user?.counters?.friends }}</span>
   </div>
 </template>
 
@@ -16,6 +16,37 @@
     name: 'FriendCard',
     props: {
       user: Object as PropType<Account>,
+    },
+    computed: {
+      getSex() {
+        let result = 'Unknown'
+        switch (this.user?.sex) {
+          case 1:
+            result = 'Female'
+            break
+          case 2:
+            result = 'Male'
+            break
+          default:
+            break
+        }
+        return result
+      },
+      getAge() {
+        let result = null
+        if (this.user?.bdate) {
+          const splitted = this.user.bdate.split('.')
+          if (splitted.length === 3) {
+            const day = Number(splitted[0])
+            const month = Number(splitted[1])
+            const year = Number(splitted[2])
+            const birthdate = new Date(year, month, day)
+            let timeDiff = Math.abs(Date.now() - birthdate.getTime())
+            result = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25)
+          }
+        }
+        return result == null ? 'Unknown' : result
+      },
     },
   })
 </script>
